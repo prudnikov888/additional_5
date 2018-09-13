5b90554b5dd00d39ecf79ee4a07320db083a3703
Andrei Prudnikau <prudnikov888@gmail.com>
2018-09-13 22:17:28 +0300

    Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let pairs = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        pairs.push(bracketsConfig[i][0] + (bracketsConfig[i][1]));
+    }
+    const n = str.length / 2;
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < pairs.length; j++) {
+            str = str.replace(pairs[j], '');
+        }
+    }
+    return str.length === 0;
+};
